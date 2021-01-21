c890c41acb88b59101ffb4aa50dd2a9f0ee3368a
siddarth <siddarth.itagi2020@gmail.com>
2021-01-20 20:03:11 -0500

    sketch.js

--- sketch.js
+++ sketch.js
@@ -75,3 +75,18 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
+function keyPressed() {
+    if(keyCode === 32) {
+        slingshot.attach(bird.body)
+    }
+}
+
+
+
+
+
+
+
+
+
